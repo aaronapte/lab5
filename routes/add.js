b8b487acb0f42c9da67e595fab8f41711cf0bfb4
Aaron Apte <aapte@ucsd.edu>
2019-02-07 11:03:21 -0800

    Initial commit

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,14 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+	data.friends.push({
+		'name': request.query.name,
+		'description': request.query.description,
+		'imageURL': 'http://lorempixel.com/500/500/people'
+	});
+	response.render('add', {
+		'name': request.query.name,
+		'description': request.query.description,
+		'imageURL': 'http://lorempixel.com/500/500/people'
+	})
  }
